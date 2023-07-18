dea5954d907d6d767d9fac72fda56be9ab56c337
David Stoner <Davidstoner216@gmail.com>
2023-07-18 00:27:18 -0300

    novodbcorretora

---
+++ Web/DaviBarreto/script.js
@@ -0,0 +1,10 @@
+
+(function () {
+    'use strict'
+    document.querySelector('[data-toggle="offcanvas"]').addEventListener('click', function () {
+        document.querySelector('.offcanvas-collapse').classList.toggle('open')
+        document.getElementById('bar1').classList.toggle("change");
+        document.getElementById('bar2').classList.toggle("change");
+        document.getElementById('bar3').classList.toggle("change");
+    })
+})()
