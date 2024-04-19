de28fd3fed8975fbcb07f6c4e76d4b654a8c41ea
David Stoner <Davidstoner216@gmail.com>
2024-04-18 22:46:49 -0300

    .

--- Web/DBCorretora/script.js
+++ Web/DBCorretora/script.js
@@ -1,11 +1,12 @@
 
 (function () {
     'use strict'
-    document.querySelector('[data-toggle="offcanvas"]').addEventListener('click', function () {
+    document.querySelector('[data-bs-toggle="offcanvas"]').addEventListener('click', function () {
         document.querySelector('.offcanvas-collapse').classList.toggle('open')
         document.getElementById('bar1').classList.toggle("change");
         document.getElementById('bar2').classList.toggle("change");
         document.getElementById('bar3').classList.toggle("change");
+        document.getElementById('menu-bar').classList.toggle("pm");
     })
 })()
 
